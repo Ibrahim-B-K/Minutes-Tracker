ff38bce9e6900d69d6505b13861f65c98f2a1be2
KEERTHI-KK24 <keerthikk2464@gmail.com>
2026-02-16 10:39:53 +0530

    added overdue email sending using smtp- brevo smtp key

---
+++ backend/api/services.py
@@ -0,0 +1,60 @@
+from django.utils import timezone
+from django.template.loader import render_to_string
+from django.core.mail import EmailMultiAlternatives
+from django.conf import settings
+
+from .models import IssueDepartment, User
+
+
+def check_and_send_overdue_emails():
+
+    today = timezone.now().date()
+
+    # Get only PENDING items that are now overdue
+    overdue_entries = IssueDepartment.objects.filter(
+        deadline_date__lt=today,
+        status="PENDING"
+    )
+
+    for entry in overdue_entries:
+
+        # 1️⃣ Change status to OVERDUE
+        entry.status = "OVERDUE"
+        entry.save()
+
+        # 2️⃣ Get department users
+        users = User.objects.filter(
+            department=entry.department,
+            role="department"
+        )
+
+        # 3️⃣ Send one email per user for THIS IssueDepartment
+        for user in users:
+
+            subject = f"Overdue Issue: {entry.issue.issue_title}"
+
+            context = {
+                "user": user,
+                "department": entry.department,
+                "issue": entry,
+            }
+
+            text_content = render_to_string(
+                "emails/overdue_issue.txt",
+                context
+            )
+
+            html_content = render_to_string(
+                "emails/overdue_issue.html",
+                context
+            )
+
+            email = EmailMultiAlternatives(
+                subject,
+                text_content,
+                settings.DEFAULT_FROM_EMAIL,
+                [user.email],
+            )
+
+            email.attach_alternative(html_content, "text/html")
+            email.send()
